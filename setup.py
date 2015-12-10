51a22fb9974a0e262796f448420aeafeb82424f7
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2015-12-10 14:48:14 +1100

    Initial setup, license, packaging etc

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='eegevt',
+      version='0.1.0',
+      description='Module for working with various EEG event file formats',
+      url='https://github.com/gjcooper/eegevt',
+      author='Gavin Cooper',
+      author_email='gjcooper@gmail.com',,
+      license='GPL v2.0',
+      packages=['eegevt'],
+      keywords=['EEG', 'event'])
