951085bdfc9413fb90efa2ab5e240cb99fc7671d
Gavin Cooper <Gavin.Cooper@newcastle.edu.au>
2015-12-10 15:09:39 +1100

    Add long_description to setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
 from setuptools import setup
 
+with open('README.md') as rfile:
+    long_description = rfile.read()
+
 setup(name='eegevt',
       version='0.1.0',
       description='Module for working with various EEG event file formats',
+      long_description=long_description
       url='https://github.com/gjcooper/eegevt',
       author='Gavin Cooper',
       author_email='gjcooper@gmail.com',,
